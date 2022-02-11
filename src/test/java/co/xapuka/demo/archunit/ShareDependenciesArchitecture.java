65a6893d5a618b1bb5805f97358f490cdc7e3422
jamoroch <jamoroch@gmail.com>
2022-02-11 16:46:39 +0100

    remove service

--- src/test/java/co/xapuka/demo/archunit/ShareDependenciesArchitecture.java
+++ src/test/java/co/xapuka/demo/archunit/ShareDependenciesArchitecture.java
@@ -7,23 +7,19 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
 
 @AnalyzeClasses(packages = "co.xapuka.demo.archunit", importOptions = {ShareDependenciesArchitecture.IgnoreClasses.class})
 class ShareDependenciesArchitecture {
 
     @ArchTest
-    static final ArchRule onlyUseShareInAModule = freeze(classes().that().resideInAPackage("..share..")
-            .should().onlyHaveDependentClassesThat().resideInAnyPackage("..app.main.amodule.."));
+    static final ArchRule onlyUseShareInAModule = classes().that().resideInAPackage("..share..")
+            .should().onlyHaveDependentClassesThat().resideInAnyPackage("..app.main.amodule..");
 
      static class IgnoreClasses implements ImportOption {
 
         @Override
         public boolean includes(Location location) {
             if (!location.contains("co/xapuka/demo/archunit/app/Service.class")) {
-                return false;
-            }
-            if (!location.contains("BService.class")) {
                 return false;
             }
             return true;
