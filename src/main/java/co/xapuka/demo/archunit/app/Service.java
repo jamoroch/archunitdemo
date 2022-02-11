e2b0e5b62628f038b29bc2a7ef60d16c0e0cc2a2
jamoroch <jamoroch@gmail.com>
2022-02-11 16:20:03 +0100

    remove freeze

---
+++ src/main/java/co/xapuka/demo/archunit/app/Service.java
@@ -0,0 +1,10 @@
+package co.xapuka.demo.archunit.app;
+
+import co.xapuka.demo.archunit.share.amodule.ModuleUtils;
+
+public class Service {
+
+    public void foo() {
+        ModuleUtils.doSomething();
+    }
+}
--- src/test/java/co/xapuka/demo/archunit/ShareDependenciesArchitecture.java
+++ src/test/java/co/xapuka/demo/archunit/ShareDependenciesArchitecture.java
@@ -1,5 +1,7 @@
 package co.xapuka.demo.archunit;
 
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.core.importer.Location;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
@@ -7,12 +9,24 @@ import com.tngtech.archunit.lang.ArchRule;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
 
-@AnalyzeClasses(packages = "co.xapuka.demo.archunit")
+@AnalyzeClasses(packages = "co.xapuka.demo.archunit", importOptions = {ShareDependenciesArchitecture.IgnoreClasses.class})
 class ShareDependenciesArchitecture {
 
     @ArchTest
-    static final ArchRule onlyUseShareInAModule =
-            freeze(classes().that().resideInAPackage("..share..").should().onlyHaveDependentClassesThat().resideInAnyPackage(
-                    "..app.main.amodule.."));
+    static final ArchRule onlyUseShareInAModule = freeze(classes().that().resideInAPackage("..share..")
+            .should().onlyHaveDependentClassesThat().resideInAnyPackage("..app.main.amodule.."));
 
+     static class IgnoreClasses implements ImportOption {
+
+        @Override
+        public boolean includes(Location location) {
+            if (!location.contains("co/xapuka/demo/archunit/app/Service.class")) {
+                return false;
+            }
+            if (!location.contains("BService.class")) {
+                return false;
+            }
+            return true;
+        }
+    }
 }
